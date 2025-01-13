f36b3ae8feba6145112a2dae37a4652c0b72a8c9
John Doe <johndoe@email.com>
2025-01-13 05:52:01 +0000

    created routes, API test, nodemailer configured

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
+import transporter from "./nodemailer.js";
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -33,6 +34,16 @@ export const register = async (req, res) => {
       sameSite: process.env.NODE_ENV === "procution" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+
+    //Sending welcome email
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: 'Welcome to nil1143',
+      text: `Welcome to nil1143 website. Your account has been created with email address: ${email}`
+    }
+
+    await transporter.sendMail(mailOptions)
 
     return res.json({ success: true });
   } catch (error) {
