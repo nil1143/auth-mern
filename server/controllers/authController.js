1b9a396609cd5049d1350f17cf8da58fd796bcc4
John Doe <johndoe@email.com>
2025-01-13 01:49:48 +0000

    created controller function for user registration

---
+++ server/controllers/authController.js
@@ -0,0 +1,38 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    // check for missing details
+    return res.json({ success: false, message: "Missing details" });
+  }
+
+  try {
+    const existingUser = await userModel.findOne({ email }); // check for existing email
+
+    if (existingUser) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10); // encrypt password
+
+    const user = new userModel({ name, email, password: hashedPassword }); // save new user
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {      // generate JWT token
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "procution" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
