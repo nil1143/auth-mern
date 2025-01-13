291af7ac6f0f006775b64fa1a6be90979041603c
John Doe <johndoe@email.com>
2025-01-13 02:14:10 +0000

    created controller function for user login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -22,7 +22,8 @@ export const register = async (req, res) => {
     const user = new userModel({ name, email, password: hashedPassword }); // save new user
     await user.save();
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {      // generate JWT token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      // generate JWT token
       expiresIn: "7d",
     });
 
@@ -34,5 +35,44 @@ export const register = async (req, res) => {
     });
   } catch (error) {
     res.json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.json({
+      success: false,
+      message: "Email and password are required",
+    });
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "Invalid email" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password); // compare password
+
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid password" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {    // generate JWT token
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "procution" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.json({ success: true });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
   }
 };
