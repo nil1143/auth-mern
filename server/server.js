bb83b07f68c7953a2ded451c371f1319fe954fbe
John Doe <johndoe@email.com>
2025-01-13 00:46:30 +0000

    backend and DB setup, installed dependencies, created UserSchema

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
-import express from "expres";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
 
 const app = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
+connectDB();
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({credentials: true}))
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true }));
 
-app.listen(port)
+app.get("/", (req, res) => res.send("API working"));
+
+app.listen(port, () => console.log(`Server started on PORT: ${port}`));
