07a44e3d01d06afe349978e9b70f4b84870a09a0
John Doe <johndoe@email.com>
2025-01-17 22:53:45 +0000

    created controller for authentication, pass reset otp req, otp verification and pass reset, routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,6 +5,9 @@ import {
   logout,
   sendVerifyOtp,
   verifyEmail,
+  isAuthenticated,
+  sendResetOtp,
+  resetPassword,
 } from "../controllers/authController.js";
 import userAuth from "../middleware/userAuth.js";
 
@@ -15,5 +18,8 @@ authRouter.post("/login", login);
 authRouter.post("/logout", logout);
 authRouter.post("/send-verify-otp", userAuth, sendVerifyOtp);
 authRouter.post("/verify-account", userAuth, verifyEmail);
+authRouter.post("/is-auth", userAuth, isAuthenticated);
+authRouter.post("/send-reset-otp", sendResetOtp);
+authRouter.post("/reset-password", resetPassword);
 
 export default authRouter;
